4a975c7d4dc041c11988eb0b9f24b7a405b407dc
Hameedarh <ajibadeolayimika@gmail.com>
2025-07-18 15:12:01 +0100

    chore:editing the pictures

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import contact from '../assets/img/w1.jpg'
+import contact from '../assets/img/ww2.png'
 import { useForm, ValidationError } from '@formspree/react';
 
 function ContactForm() {
